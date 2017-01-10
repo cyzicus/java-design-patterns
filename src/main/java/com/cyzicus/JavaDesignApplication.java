c435e6f58ec910a12681b5cad2c5e57e2d85bf73
Jason C Miller <cyzicusgit@gmail.com>
2017-01-10 12:45:12 -0700

    initial commit

---
+++ src/main/java/com/cyzicus/JavaDesignApplication.java
@@ -0,0 +1,12 @@
+package com.cyzicus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDesignApplication.class, args);
+	}
+}
