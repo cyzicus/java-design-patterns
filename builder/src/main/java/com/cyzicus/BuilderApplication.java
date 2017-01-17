ce628048c7e9261fe753cd502a3db61ac90eb8c0
Jason C Miller <cyzicusgit@gmail.com>
2017-01-16 20:05:58 -0700

    add builder classes for models

--- builder/src/main/java/com/cyzicus/BuilderApplication.java
+++ builder/src/main/java/com/cyzicus/BuilderApplication.java
@@ -1,5 +1,7 @@
 package com.cyzicus;
 
+import com.cyzicus.model.Meal;
+import com.cyzicus.model.MealBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +10,20 @@ public class BuilderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuilderApplication.class, args);
+		runBuilderPatternDemo();
+	}
+
+	private static void runBuilderPatternDemo() {
+		MealBuilder mealBuilder = new MealBuilder();
+
+		Meal vegMeal = mealBuilder.prepareVegMeal();
+		System.out.println("Veg Meal");
+		vegMeal.shotItems();
+		System.out.println("Total Cost: " + vegMeal.getCost());
+
+		Meal nonVegMeal = mealBuilder.prepareNonVegMeal();
+		System.out.println("\n\nNon-Veg Meal");
+		nonVegMeal.shotItems();
+		System.out.println("Total Cost: " + nonVegMeal.getCost());
 	}
 }
