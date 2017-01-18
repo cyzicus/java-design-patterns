367e0278bd6608671932bd78e4572f120cbd5202
Jason C Miller <cyzicusgit@gmail.com>
2017-01-18 13:03:18 -0700

    finishes prototype pattern

---
+++ prototype/src/main/java/com/cyzicus/Circle.java
@@ -0,0 +1,13 @@
+package com.cyzicus;
+
+public class Circle extends Shape {
+
+    public Circle() {
+        this.type = "Circle";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Inside Circle::draw() method");
+    }
+}
---
+++ prototype/src/main/java/com/cyzicus/Rectangle.java
@@ -0,0 +1,13 @@
+package com.cyzicus;
+
+public class Rectangle extends Shape {
+
+    public Rectangle() {
+        this.type = "Rectangle";
+    }
+
+    @Override
+    void draw() {
+        System.out.println("Inside Rectangle::draw() method");
+    }
+}
